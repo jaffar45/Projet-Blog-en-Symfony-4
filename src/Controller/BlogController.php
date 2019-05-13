cae9a99a95b047a4eb9c2ec271134fd2f4aa3a6a
Jaffar Mallem <jmtrading@live.com>
2019-05-13 15:37:44 +0200

    Modification of BogContoller to get all articles in article table

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,19 +6,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
 
 
 class BlogController extends AbstractController
 {
     /**
+     * Show all row from article's entity
+     *
      * @Route("/blog", name="blog_index")
+     * @return Response A response instance
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'owner' => 'Jaffar',
-        ]);
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/index.html.twig',
+            ['articles' => $articles]
+        );
     }
+
 
     /**
      * @Route("blog/show/{slug}", requirements={"slug"="^[a-z0-9](-?[a-z0-9])*$"}, name="blog_show")

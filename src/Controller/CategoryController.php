246e3608f7eeb0f8614016f0bf2e2068b10bdb97
Jaffar Mallem <jmtrading@live.com>
2019-05-20 15:52:24 +0200

    Creation of new category using form

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +13,29 @@ class CategoryController extends AbstractController
     /**
      * @Route("/category", name="category")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $category = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('category');
+        }
+
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'form' => $form->CreateView()
         ]);
     }
 }

111731de390978b8fc052bb07c9a43dedd200515
Jaffar Mallem <jmtrading@live.com>
2019-05-21 11:54:25 +0200

    Addition of Tags to Articles vue

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -4,18 +4,20 @@ namespace App\Controller;
 
 use App\Entity\Tag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class TagController extends AbstractController
 {
     /**
-     * @Route("/tag/{name}", name="tag")
-     * @param Tag $tag
+     * @Route("/tag/{name}", name="show_tag")
+     * @ParamConverter("tag", class="App\Entity\Tag")
      */
-    public function index(Tag $tag)
+    public function index(Tag $tag) :Response
     {
         return $this->render('tag/index.html.twig', [
-            'articles' => $tag->getArticles(),
+            'tag' => $tag,
         ]);
     }
 }
